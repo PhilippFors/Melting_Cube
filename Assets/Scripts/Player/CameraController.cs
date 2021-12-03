260b995d447d9107227e6fc016ad6d67d1e8291c
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2021-12-03 23:01:36 +0100

    experimental camera controller

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,16 +1,23 @@
-
 using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private bool followPlayer = true;
     [SerializeField] private float xOffset;
     [SerializeField] private float yOffset;
-
+    [SerializeField] private float edgePadding = 100f;
+    [SerializeField] private float speed = 5f;
+    private Camera cam;
+    private int ScreenWidth => Screen.width;
+    private int ScreenHeight => Screen.height;
+    
     private void Start()
     {
+        cam = GetComponent<Camera>();
         StartCoroutine(FindPlayer());
+        transform.position = new Vector3(xOffset, transform.position.y, transform.position.z);
     }
 
     void Update()
@@ -18,9 +25,34 @@ public class CameraController : MonoBehaviour
         if (!player) {
             return;
         }
+        
+        if (followPlayer) {
+            transform.position = new Vector3(player.position.x + xOffset, player.position.y + yOffset, player.position.z);
+            return;
+        }
+        
+        var newPos = Vector3.zero;
+        var screenPoint = cam.WorldToScreenPoint(player.position);
 
-        var newPos = new Vector3(player.position.x + xOffset, player.position.y + yOffset, player.position.z);
-        transform.position = newPos;
+        if (screenPoint.x <= edgePadding) {
+            newPos += -Vector3.forward * Time.deltaTime * speed;
+        }
+
+        if (screenPoint.x >= ScreenWidth - edgePadding) {
+            newPos += Vector3.forward * Time.deltaTime * speed;
+        }
+
+        if (screenPoint.y <= edgePadding) {
+            newPos += Vector3.down * Time.deltaTime * speed;
+        }
+
+        if (screenPoint.y >= ScreenHeight - edgePadding) {
+            newPos += Vector3.up * Time.deltaTime * speed;
+        }
+        
+        newPos = new Vector3(newPos.x, newPos.y, newPos.z);
+
+        transform.position += newPos;
     }
 
     // private void OnValidate()
